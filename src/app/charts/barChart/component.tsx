89209db6de5f8afc6add56eee0dae80af4e31018
Mugu <mugurel.socaciu@gmail.com>
2019-12-14 19:39:14 +0200

    Added HighCharts

--- src/app/charts/barChart/component.tsx
+++ src/app/charts/barChart/component.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+ 
+const options: Highcharts.Options = {
+  title: {
+      text: 'My chart'
+  },
+  series: [{
+      type: 'line',
+      data: [1, 2, 3,6,2,8,3,6,4,3,2,0]
+  }]
+}
 
 export class BarChart extends React.Component<any, any> {
   render() {
-    return <div>BarcHART</div>;
+    return <HighchartsReact 
+    highcharts={Highcharts}
+    options={options}
+  />}
   }
-}
