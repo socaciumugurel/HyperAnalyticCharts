c72d0acb1c94f748a24c67e757fbdf5a984b1772
Mugurel <mugurel.socaciu@gmail.com>
2020-03-10 19:11:29 +0200

    config devToolsExtension

--- src/index.js
+++ src/index.js
@@ -7,13 +7,19 @@ import logger from "redux-logger";
 
 // redux-thunk is a middleware that lets you dispatch async actions
 import thunk from "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import App from "./App";
 import rootReducer from "./reducers/rootReducer";
 import { register } from "./serviceWorker";
 
 const middleware = applyMiddleware(thunk, logger);
-const store = createStore(rootReducer, middleware);
+const store = createStore(
+  rootReducer,
+  compose(
+    middleware,
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
